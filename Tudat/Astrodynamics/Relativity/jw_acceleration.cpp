6431d6081c8c2afd0ee60416a6ee5421c5ebfdf7
David Roland <jwestin64@gmail.com>
2019-08-25 15:28:44 +0200

    changed a sign

--- Tudat/Astrodynamics/Relativity/jw_acceleration.cpp
+++ Tudat/Astrodynamics/Relativity/jw_acceleration.cpp
@@ -13,13 +13,13 @@ namespace jw {
         Eigen::Vector3d pos_sa = state_sa.segment(0,3);
         Eigen::Vector3d vel_sa = state_sa.segment(3,3);
 
-        double c = tudat::physical_constants::SPEED_OF_LIGHT;
+        const double c = tudat::physical_constants::SPEED_OF_LIGHT;
         double r = pos_sa.norm();
 
         double mu_c2_r3 = mu_actor / (c*c*r*r*r);
 
         return mu_c2_r3 * (
-            (4.0*mu_actor/r + vel_sa.dot(vel_sa)) * pos_sa +
+            (4.0*mu_actor/r - vel_sa.dot(vel_sa)) * pos_sa +
             4*(pos_sa.dot(vel_sa)) * vel_sa
         );
 
